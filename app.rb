1e034c970ed5045fc836964447a16b99d6df6f04
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-23 22:16:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:num1/:num2' do
-    @answer = params[:num1] * params[:num2]
+    @answer = params[:num1].to_i * params[:num2].to_i
     erb :multiply
   end
 
