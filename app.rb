6f2cc42e7107aa47229df03e48389c7b48234442
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-23 21:42:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,5 +18,11 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}!"
   end
+  
+  get "/multiply/:num1/:num2" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
+
 
 end
